11d54169560d6e9a5fcf630ec7f3ccc94557a2bd
Javier Gutierrez <jjavierweb@outlook.com>
2023-01-06 13:14:35 -0500

    Modified error message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,7 @@
 <div class=" h-[calc(100vh-150px)] mx-auto p-4 flex justify-center items-center">
 	<div class="flex flex-col align-middle place-content-center">
 		<h1 class="text-2xl sm:text-4xl uppercase text-center">
-			An error has occurred. Please try again later.
+			An error has occurred. Please try again later or contact support for assistance.
 		</h1>
 		{#if $page.error?.message === 'Not Found'}
 			<img src={ErrorImage} alt="Error Message Page not found" class="align-middle items-center" />
