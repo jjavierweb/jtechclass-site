5139722ef4351bd5b4819ed8b7401188b661c5ee
Javier Gutierrez <jjavierweb@outlook.com>
2023-01-06 12:50:02 -0500

    Added error page handler

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ErrorImage from '$lib/assets/images/ErrorImage.svg';
+</script>
+
+<div class=" h-[calc(100vh-150px)] mx-auto p-4 flex justify-center items-center">
+	<div class="flex flex-col align-middle place-content-center">
+		<h1 class="text-2xl sm:text-4xl uppercase text-center">
+			An error has occurred. Please try again later.
+		</h1>
+		{#if $page.error?.message === 'Not Found'}
+			<img src={ErrorImage} alt="Error Message Page not found" class="align-middle items-center" />
+		{/if}
+	</div>
+</div>
