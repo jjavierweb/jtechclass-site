c39987ecbc0447c4370f7e3e8c88557c3c3d6934
Javier Gutierrez <jjavierweb@outlook.com>
2023-07-20 23:58:03 -0400

    fixed name on the about us page

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <svelte:head>
-	<title>About Us - Javier Aviation</title>
+	<title>About Us - JTechClass</title>
 	<meta
 		name="description"
-		content="At Javier Aviation, our mission is to provide small businesses with top-quality IT solutions and support that enable them to operate efficiently and effectively"
+		content="At JTechClass, our mission is to provide small businesses with top-quality IT solutions and support that enable them to operate efficiently and effectively"
 	/>
 </svelte:head>
 <!-- Section of Hero -->
@@ -49,8 +49,8 @@
 		<div class="mt-8">
 			<h2 class="text-3xl font-semibold md:text-5xl">Our Mission:</h2>
 			<p class="text-justify mt-4 text-lg">
-				At Javier Aviation, our mission is to provide small businesses with top-quality IT solutions
-				and support that enable them to operate efficiently and effectively. We are committed to
+				At JTechClass, our mission is to provide small businesses with top-quality IT solutions and
+				support that enable them to operate efficiently and effectively. We are committed to
 				delivering exceptional customer service and building long-term partnerships with our
 				clients. By leveraging our expertise and cutting-edge technology, we aim to help small
 				businesses succeed in today's rapidly changing business landscape.
@@ -62,19 +62,19 @@
 		<div class="mt-8">
 			<h2 class="text-3xl font-semibold md:text-5xl">Our Vision:</h2>
 			<p class="text-justify mt-4 text-lg">
-				Javier Aviation is a small business dedicated to helping other small businesses with their
-				IT needs. Our vision is to become the go-to IT solutions provider for small businesses in
-				our community and beyond. We believe that small businesses are the backbone of the economy,
-				and we are passionate about helping them succeed. We understand the unique challenges that
-				small businesses face when it comes to IT, and we are committed to providing affordable,
-				reliable, and user-friendly solutions that allow them to focus on what they do best: growing
-				their business. To achieve this vision, we will strive to consistently deliver excellent
-				customer service and technical support to our clients. We will stay up to date with the
-				latest technologies and best practices in the industry, and we will always go the extra mile
-				to ensure that our clients are satisfied with our services. In the long term, we hope to
-				expand our reach and help even more small businesses with their IT needs. We envision a
-				future where every small business has access to the resources and support they need to
-				thrive, and we are committed to playing a key role in making that a reality.
+				JTechClass is a small business dedicated to helping other small businesses with their IT
+				needs. Our vision is to become the go-to IT solutions provider for small businesses in our
+				community and beyond. We believe that small businesses are the backbone of the economy, and
+				we are passionate about helping them succeed. We understand the unique challenges that small
+				businesses face when it comes to IT, and we are committed to providing affordable, reliable,
+				and user-friendly solutions that allow them to focus on what they do best: growing their
+				business. To achieve this vision, we will strive to consistently deliver excellent customer
+				service and technical support to our clients. We will stay up to date with the latest
+				technologies and best practices in the industry, and we will always go the extra mile to
+				ensure that our clients are satisfied with our services. In the long term, we hope to expand
+				our reach and help even more small businesses with their IT needs. We envision a future
+				where every small business has access to the resources and support they need to thrive, and
+				we are committed to playing a key role in making that a reality.
 			</p>
 		</div>
 		<img
